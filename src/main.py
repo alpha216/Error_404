c939150bd0bb3388861a064bd3bd318d71dee4e9
Minsung Kim <alphacar@Minsungui-MacBookPro.local>
2023-01-26 23:41:17 -0600

    Incomplete

--- src/main.py
+++ src/main.py
@@ -7,17 +7,16 @@ brain=Brain()
 
 # Robot configuration code
 controller_1 = Controller(PRIMARY)
-Intake = Motor(Ports.PORT18, GearSetting.RATIO_36_1, False)
-Shoot_1 = Motor(Ports.PORT20, GearSetting.RATIO_6_1, True)
-Shoot_2 = Motor(Ports.PORT19, GearSetting.RATIO_6_1, True)
-Shoot = MotorGroup(Shoot_1, Shoot_2)
-Left_1 = Motor(Ports.PORT6, GearSetting.RATIO_36_1, False)
-Left_2 = Motor(Ports.PORT16, GearSetting.RATIO_36_1, False)
+Intake = Motor(Ports.PORT13, GearSetting.RATIO_36_1, True)
+Shoot = Motor(Ports.PORT1, GearSetting.RATIO_36_1, True)
+Roller = Motor(Ports.PORT15, GearSetting.RATIO_36_1, True)
+# Shoot = MotorGroup(Shoot_1, Shoot_2)
+Left_1 = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False)
+Left_2 = Motor(Ports.PORT16, GearSetting.RATIO_6_1, False)
 Left = MotorGroup(Left_1, Left_2)
-Right_1 = Motor(Ports.PORT5, GearSetting.RATIO_36_1, True)
-Right_2 = Motor(Ports.PORT15, GearSetting.RATIO_36_1, True)
+Right_1 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
+Right_2 = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)
 Right = MotorGroup(Right_1, Right_2)
-Roller = Motor(Ports.PORT13, GearSetting.RATIO_36_1, True)
 
 
 # wait for rotation sensor to fully initialize
@@ -34,18 +33,21 @@ def L2():
     Shoot.spin(FORWARD)
     while controller_1.buttonL2.pressing():
         wait(5, MSEC)
+    Shoot.stop()
     
-def R1():
-    Roller.spin(FORWARD)
-    while controller_1.buttonR1.pressing():
-        wait(5, MSEC)
-    Roller.stop()
+# def R1():
+#     Roller.spin(FORWARD)
+#     while controller_1.buttonR1.pressing():
+#         wait(5, MSEC)
+#     Roller.stop()
     
 def R2():
-    Roller.spin(REVERSE)
-    while controller_1.buttonR2.pressing():
-        wait(5, MSEC)
-    Roller.stop()
+    # Roller.spin(REVERSE)
+    Roller.spin_for(FORWARD, -50, DEGREES, wait=True)
+    wait(20, MSEC)
+    Roller.spin_for(FORWARD, 50, DEGREES, wait=True)
+
+    # Roller.stop()
     
 def ax3():
     Left.set_velocity(controller_1.axis3.position(), PERCENT)
@@ -65,7 +67,7 @@ def ax1():
 # Create Controller callback events
 controller_1.buttonL1.pressed(L1)
 controller_1.buttonL2.pressed(L2)
-controller_1.buttonR1.pressed(R1)
+# controller_1.buttonR1.pressed(R1)
 controller_1.buttonR2.pressed(R2)
 controller_1.axis3.changed(ax3)
 controller_1.axis1.changed(ax1)
@@ -74,8 +76,8 @@ controller_1.axis1.changed(ax1)
 # 15 msec delay to ensure events get registered
 wait(15, MSEC)
 
-Intake.set_velocity(1000, RPM)
-Shoot_1.set_velocity(100, PERCENT)
-Shoot_2.set_velocity(100, PERCENT)
+Intake.set_velocity(70, PERCENT)
+Shoot.set_velocity(100, PERCENT)
+# Shoot_2.set_velocity(100, PERCENT)
 
 
